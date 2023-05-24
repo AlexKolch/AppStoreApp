d5efaec939607137a290018e74abb3140d4084d1
Alexey Kolch <kolychenkov94@mail.ru>
2023-05-24 13:06:01 +0400

    layer done

---
+++ AppStoreApp/Controller/Apps/AppsController.swift
@@ -0,0 +1,36 @@
+//
+//  AppsController.swift
+//  AppStoreApp
+//
+//  Created by Алексей Колыченков on 24.05.2023.
+//
+
+import UIKit
+
+//private let reuseIdentifier = "Cell"
+
+class AppsController: BaseListController, UICollectionViewDelegateFlowLayout {
+
+    private let cellId = "cellID"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .yellow
+        collectionView.register(AppsGroupCell.self, forCellWithReuseIdentifier: cellId)
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
+    }
+
+}
