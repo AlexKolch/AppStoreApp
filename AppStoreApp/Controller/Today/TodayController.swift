9d58b789432c0b75f68b72f92b1f9cabdc493e4d
Alexey Kolch <kolychenkov94@mail.ru>
2023-07-16 22:43:28 +0300

    Auto Layout Constraint Animations
    - Auto Layout Constraint Animations instead frame animations
    - add close button

--- AppStoreApp/Controller/Today/TodayController.swift
+++ AppStoreApp/Controller/Today/TodayController.swift
@@ -10,7 +10,12 @@ import UIKit
 class TodayController: BaseListController {
     private let cellId = "CellId"
     fileprivate var startingFrame: CGRect?
-    private var appFullscreenController: UIViewController!
+    private var appFullscreenController: AppFullScreenController!
+
+    var topConstraint: NSLayoutConstraint?
+    var leadingConstraint: NSLayoutConstraint?
+    var widthConstraint: NSLayoutConstraint?
+    var heightConstraint: NSLayoutConstraint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,15 +48,31 @@ class TodayController: BaseListController {
         guard let cell = collectionView.cellForItem(at: indexPath) else {return} ///Объект ячейки по индексу
 
         guard let startingFrame = cell.superview?.convert(cell.frame, to: nil) else {return}
+
         self.startingFrame = startingFrame
 
-        redView.frame = startingFrame
+        ///auto layout constraint animations in angle screen
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        topConstraint = redView.topAnchor.constraint(equalTo: view.topAnchor, constant: startingFrame.origin.y)
+        leadingConstraint = redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: startingFrame.origin.x)
+        widthConstraint = redView.widthAnchor.constraint(equalToConstant: startingFrame.width)
+        heightConstraint = redView.heightAnchor.constraint(equalToConstant: startingFrame.height)
+        [topConstraint, leadingConstraint, widthConstraint, heightConstraint].forEach({$0?.isActive = true})
+        self.view.layoutIfNeeded()
+
+       // redView.frame = startingFrame
         redView.layer.cornerRadius = 16
 
         ///Анимация открытия ячейки на весь экран
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut) {
 
-            redView.frame = self.view.frame
+            self.topConstraint?.constant = 0
+            self.leadingConstraint?.constant = 0
+            self.widthConstraint?.constant = self.view.frame.width
+            self.heightConstraint?.constant = self.view.frame.height
+            self.view.layoutIfNeeded()
+//            redView.frame = self.view.frame
+
             ///скрываем tabBar
             self.tabBarController?.tabBar.transform = CGAffineTransform(translationX: 0, y: 50)
             self.tabBarController?.tabBar.isHidden = true
@@ -62,7 +83,17 @@ class TodayController: BaseListController {
         ///Анимация закрытия ячейки и возвращение к startingFrame
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: .curveEaseOut) {
 
-            gesture.view?.frame = self.startingFrame ?? .zero
+            self.appFullscreenController.tableView.contentOffset = .zero
+            //self.appFullscreenController.tableView.scrollToRow(at: [0, 0], at: .top, animated: true) ///второй способ свернуть
+           // gesture.view?.frame = self.startingFrame ?? .zero ///this frame code is bad
+            guard let startingFrame = self.startingFrame else {return}
+
+            self.topConstraint?.constant = startingFrame.origin.y
+            self.leadingConstraint?.constant = startingFrame.origin.x
+            self.widthConstraint?.constant = startingFrame.width
+            self.heightConstraint?.constant = startingFrame.height
+            self.view.layoutIfNeeded()
+
             ///возвращаем tabBar
             self.tabBarController?.tabBar.transform = CGAffineTransform(translationX: 0, y: 70)
             self.tabBarController?.tabBar.isHidden = false
